d2b2ffa9fbb717edd628e807ebc1780dd24f5c43
wanjwoo <116162460+wanjinwoo@users.noreply.github.com>
2023-11-07 17:47:02 +0000

    wallet

--- Athena3-Hub-athena-frontend/src/components/NavBar.js
+++ Athena3-Hub-athena-frontend/src/components/NavBar.js
@@ -1,16 +1,18 @@
-import * as allList from "../list.js";
-import { Menu } from 'antd';
-import { Link } from 'react-router-dom';
+import * as allList from "../list.js"
+import { Menu } from 'antd'
+import { Link } from 'react-router-dom'
+import Wallet from './Wallet.js'
 function NavBar (params) {
-  const navList = allList.navList;
+  const navList = allList.navList
   return (
     <div className='navbar'>
-  <div className='navbar-logo'>logo</div>
-    <Menu mode="horizontal">
-    {navList.map(item=> (<Menu.Item  key={item.id}><Link to={item.path}>{item.name}</Link></Menu.Item>))}
-    </Menu>
-    <div className='navbar-wallet '>wallet</div>
+      <div className='navbar-logo'>logo</div>
+      <Menu className="navbar-menu" mode="horizontal">
+        {navList.map(item => (<Menu.Item key={item.id}><Link to={item.path}>{item.name}</Link></Menu.Item>))}
+      </Menu>
+      {/* <div className='navbar-wallet '>wallet</div> */}
+      <Wallet/>
     </div >
-  );
+  )
 }
-export default NavBar;
+export default NavBar
