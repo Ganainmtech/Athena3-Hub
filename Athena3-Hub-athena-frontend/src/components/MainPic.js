ccce7a01123861d22f716bc76c393d5dc9832f95
wanjwoo <116162460+wanjinwoo@users.noreply.github.com>
2023-11-10 10:04:28 +0000

    add name

--- Athena3-Hub-athena-frontend/src/components/MainPic.js
+++ Athena3-Hub-athena-frontend/src/components/MainPic.js
@@ -1,4 +1,4 @@
-import logo from "../assets/logo.png"
+import logo from "../assets/nameWhite.png"
 import React from 'react';
 import { Button, Flex  } from 'antd';
 function MainPic () {
@@ -7,7 +7,7 @@ function MainPic () {
       <div className='mainPic' >
         <div className="title"><img src={logo} alt="Logo" className="mainPic-logo" /></div>
         <div className="mainPic-subtitle">
-          <div>Unlocking your potential</div>
+          <h1 className="subtitle1">Unlock your potential</h1>
           <div>Learn from women, grow with women, build for women</div>
           </div>
           <Flex gap="large" wrap="wrap">
