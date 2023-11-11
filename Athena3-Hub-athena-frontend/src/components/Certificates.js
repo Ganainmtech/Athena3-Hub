55d851911f24c7e734b8be7a64fbb829899e1cfc
wanjwoo <116162460+wanjinwoo@users.noreply.github.com>
2023-11-11 23:46:52 +0000

    Update Certificates.js

--- Athena3-Hub-athena-frontend/src/components/Certificates.js
+++ Athena3-Hub-athena-frontend/src/components/Certificates.js
@@ -1,22 +1,25 @@
 import * as allList from "../list.js"
+import React from 'react'
+import { Button} from 'antd'
+
 function Certificates () {
   const list = allList.certificatesList
+
   return (
     <div className='certificates'>
       {list.map(item => (
         <Button
           key={item.id}
           block
-          className='certificates-list'
+          className="certificates-list"
         >
-          {/* <div>{item.icon}</div> */}
-          <div>
-            <div className='certificates-name'>{item.name}</div>
-            <div className='certificates-subname'>{item.subName}</div>
-          </div>
-        ))}
-      </div>
-    </>
+          <div className="certificates-name">{item.name}</div>
+          <div className="certificates-subname">{item.subName}</div>
+          {/* id is not displayed on the button */}
+        </Button>
+      ))}
+    </div>
   )
 }
-export default Certificates;
+
+export default Certificates
