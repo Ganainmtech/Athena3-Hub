914a3ac22d58374fb8b14e0dd41c05cda385c252
Lucia <stupidlifewww@foxmail.com>
2023-11-11 23:14:54 +0000

    update fix menu

--- Athena3-Hub-athena-frontend/src/components/NavBar.js
+++ Athena3-Hub-athena-frontend/src/components/NavBar.js
@@ -3,12 +3,37 @@ import { Menu } from 'antd';
 import { Link } from 'react-router-dom';
 import Wallet from './Wallet.js';
 import LogoImage from '../assets/TransperantLogo.png';
+import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 
 function NavBar (params) {
   const navList = allList.navList;
+  const navigate = useNavigate();
+  const [isFixed, setIsFixed] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const offsetToFix = 100; // 根据需要调整固定导航栏的滚动偏移
+
+      if (scrollPosition > offsetToFix && !isFixed) {
+        setIsFixed(true);
+      } else if (scrollPosition <= offsetToFix && isFixed) {
+        setIsFixed(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // 清除监听器，防止内存泄漏
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [isFixed]);
+
   return (
-    <div className='navbar'>
-      <div>
+    <div className={`navbar ${isFixed ? 'fixed' : ''}`}>
+      <div onClick={() => navigate('/')}>
         <img className='navbar-logo' src={LogoImage} alt="Logo" />
       </div>
       <Menu className="navbar-menu" mode="horizontal">
